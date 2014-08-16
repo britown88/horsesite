bc9b973bf7d9b453825fa633ada47b2fb63f72a1
Brandon Townsend <p4r4digm@gmail.com>
2014-08-15 22:58:04 -0700

    Removed unecessary shit

--- dhsite/urls.py
+++ dhsite/urls.py
@@ -3,7 +3,6 @@ from django.conf import settings
 
 urlpatterns = patterns('',
 	(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
-	(r'^gitstats/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'srv/django/dapperhat/templates/dhsite/gitstats/' }),
+	#(r'^gitstats/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'srv/django/dapperhat/templates/dhsite/gitstats/' }),
 	url(r'^$', 'dhsite.views.index'),
-	url(r'^zendesk/$','dhsite.views.zendesk'),
 )

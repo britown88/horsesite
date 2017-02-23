6d6377974635ef602a6a367c3eacdc12126255c3
Brandon Townsend <p4r4digm@gmail.com>
2017-02-22 21:55:03 -0700

    added factorio map

--- dhsite/urls.py
+++ dhsite/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 urlpatterns = patterns('',
 	(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
 	#(r'^gitstats/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'srv/django/dapperhat/templates/dhsite/gitstats/' }),
+	(r'^factorio/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'srv/django/dapperhat/templates/factorio/' }),
 	url(r'^$', 'dhsite.views.index'),
 	url(r'^dillodash/?$', 'dhsite.views.ddash'),
     url(r'^pointbuy/?$', 'dhsite.views.pointbuy'),

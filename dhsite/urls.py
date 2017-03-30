89b48c41972571be068890f92c47ef1b88604ef2
Brandon Townsend <root@mail.studio-horse.com>
2017-03-30 15:36:35 -0400

    updated key

--- dhsite/urls.py
+++ dhsite/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 urlpatterns = patterns('',
 	(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
 	#(r'^gitstats/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'srv/django/dapperhat/templates/dhsite/gitstats/' }),
-	(r'^factorio/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'srv/django/dapperhat/templates/factorio/' }),
+	(r'^factorio/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.BASE_DIR +  '/templates/factorio/' }),
 	url(r'^$', 'dhsite.views.index'),
 	url(r'^dillodash/?$', 'dhsite.views.ddash'),
     url(r'^pointbuy/?$', 'dhsite.views.pointbuy'),
